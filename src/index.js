d88bf1dbce7e619b459f1b8483416de45ac7ce91
Luiz Fernando Palin Droubi <lfpdroubi@gmail.com>
2020-04-13 19:22:24 +0000

    adds toolbar to edit geometries with leaflet.draw

--- src/index.js
+++ src/index.js
@@ -328,7 +328,6 @@ LPM_PRESUMIDA, teste).done(function() {
       fullscreenControlOptions: {
         position: 'topright'
       },
-      drawControl: false,
       measureControl:true,
   });
   
@@ -1042,5 +1041,37 @@ LPM_PRESUMIDA, teste).done(function() {
   }
   
   logo.addTo(map);
+  
+  var featureGroup = L.featureGroup().addTo(map);
+
+  var drawControl = new L.Control.Draw({
+      edit: {
+          featureGroup: featureGroup
+      }
+  }).addTo(map);
+
+  map.on('draw:created', function(e) {
+
+      // Each time a feaute is created, it's added to the over arching feature group
+      featureGroup.addLayer(e.layer);
+  });
+
+  
+  // on click, clear all layers
+  document.getElementById('delete').onclick = function(e) {
+    featureGroup.clearLayers();
+  }
+
+  document.getElementById('export').onclick = function(e) {
+    // Extract GeoJson from featureGroup
+    var data = featureGroup.toGeoJSON();
+
+    // Stringify the GeoJson
+    var convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+
+    // Create export
+    document.getElementById('export').setAttribute('href', 'data:' + convertedData);
+    document.getElementById('export').setAttribute('download','data.geojson');
+  }
 
 });
