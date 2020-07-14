5b22ca324c449799407fbade2ca076db15b7d76b
Luiz Fernando Palin Droubi <lfpdroubi@gmail.com>
2020-07-14 21:12:50 +0000

    Adds sidebar.

--- src/index.js
+++ src/index.js
@@ -1308,4 +1308,11 @@ $.when(portos, aeroportos, cessoes, ocupacoes, certdisp, autobras, entregas,
       console.log(layer);
   });
   
+  var sidebar = L.control.sidebar({
+    autopan: false,       // whether to maintain the centered map point when opening the sidebar
+    closeButton: true,    // whether t add a close button to the panes
+    container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+    position: 'left',     // left or right
+}).addTo(map);
+  
 });
